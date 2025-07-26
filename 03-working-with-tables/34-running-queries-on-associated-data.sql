46e1cbfe54222291d47a342ad9a0078cca40213c
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-07-26 17:11:05 +0900

    running queries on associated data

---
+++ 03-working-with-tables/34-running-queries-on-associated-data.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+  photos (url, user_id)
+VALUES
+  ('http://two.jpg', 1),
+  ('http://25.jpg', 1),
+  ('http://36.jpg', 1),
+  ('http://754.jpg', 2),
+  ('http://35.jpg', 3),
+  ('http://256.jpg', 4);
+
+SELECT * FROM photos WHERE user_id = 4;
+
+SELECT
+  url,
+  username
+FROM
+  photos
+  JOIN users ON photos.user_id = users.id;
