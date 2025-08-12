7ec1473e6db303ffc33b9480585635b7821d6178
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-08-12 23:41:37 +0900

    change p name

--- 09-assembling-queries-with-subqueries/100-example-of-a-subquery-in-a-from.sql
+++ 09-assembling-queries-with-subqueries/100-example-of-a-subquery-in-a-from.sql
@@ -1,2 +1,2 @@
 SELECT AVG(order_count)
-FROM (SELECT user_id, COUNT(*) AS order_count FROM orders GROUP BY user_id) AS p;
+FROM (SELECT user_id, COUNT(*) AS order_count FROM orders GROUP BY user_id) AS user_order_counts;
