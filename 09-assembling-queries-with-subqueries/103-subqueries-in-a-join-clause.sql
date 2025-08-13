9ad18a50ea882dfc2b9a16761a28db60140d9ea4
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-08-13 23:50:12 +0900

    add a same result sql statement

--- 09-assembling-queries-with-subqueries/103-subqueries-in-a-join-clause.sql
+++ 09-assembling-queries-with-subqueries/103-subqueries-in-a-join-clause.sql
@@ -3,3 +3,10 @@ FROM users
 JOIN (
     SELECT user_id FROM orders WHERE product_id = 3
 ) AS user_ids ON users.id = user_ids.user_id;
+
+--- same result
+SELECT first_name
+FROM users
+JOIN orders
+ON orders.user_id = users.id
+WHERE orders.product_id = 3;
